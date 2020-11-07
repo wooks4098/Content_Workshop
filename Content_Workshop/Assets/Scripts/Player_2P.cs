afe19fc933950162511784fcf6728883d777d10e
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2020-11-07 20:02:08 +0900

    곰돌이 애니메이션추가

--- Content_Workshop/Assets/Scripts/Player_2P.cs
+++ Content_Workshop/Assets/Scripts/Player_2P.cs
@@ -9,14 +9,18 @@ public class Player_2P : MonoBehaviour
     public int JumpCount;//점프카운트
     public LayerMask PlatFormCheck_Layer;//플랫폼 체크 레이어
     public GameObject TileCheck; //플랫폼 체크 위치
-    Rigidbody2D rigid;
-    float timecheck = 0;
+    float Jump_timecheck = 0;//점프 쿨타임용
 
+
+    Rigidbody2D rigid;//물리
+    Animator anim;//애니메이션
 
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
     }
 
     private void FixedUpdate()
@@ -29,17 +33,25 @@ public class Player_2P : MonoBehaviour
 
     void Move()
     {
-        timecheck += Time.deltaTime;
+        if (rigid.velocity.y == 0 && rigid.velocity.x == 0)
+        {
+            anim.SetBool("IsWalk", false);
+            anim.SetBool("IsJump", false);
+            anim.SetBool("Is2Jump", false);
+        }
+        Jump_timecheck += Time.deltaTime;
         //이동
         if (Input.GetKey(KeyCode.J))
         {
             gameObject.transform.position += Vector3.left * Speed * Time.deltaTime;
             gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+            anim.SetBool("IsWalk", true);
         }
         if (Input.GetKey(KeyCode.L))
         {
             gameObject.transform.position += Vector3.right * Speed * Time.deltaTime;
             gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+            anim.SetBool("IsWalk", true);
         }
         //점프
         if (rigid.velocity.y <= 0)//레이캐스트를 사용하여 타일에 닿았는지 확인
@@ -47,12 +59,17 @@ public class Player_2P : MonoBehaviour
             if (Physics2D.Raycast(TileCheck.transform.position, Vector3.down, 0.15f, PlatFormCheck_Layer))
                 JumpCount = 2;
         }
-        if (Input.GetKey(KeyCode.RightShift) && JumpCount >= 1 && timecheck>= 0.3f)
+        if (Input.GetKey(KeyCode.RightShift) && JumpCount >= 1 && Jump_timecheck >= 0.3f)
         {
+            if(JumpCount == 2)
+                anim.SetBool("IsJump", true);
+            else
+                anim.SetBool("Is2Jump", true);
+
             rigid.velocity = Vector2.zero;
             rigid.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
             JumpCount--;
-            timecheck = 0;
+            Jump_timecheck = 0;
 
         }
         Debug.DrawRay(TileCheck.transform.position, Vector2.down * 0.15f, Color.red);//레이케스트 보여주는 코드

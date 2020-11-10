9faae07054330b1dc69ec5e3d477989de22971d4
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2020-11-11 00:00:58 +0900

    스위치 문 수정
    Red Blue

--- Content_Workshop/Assets/Scripts/Object/Switch.cs
+++ Content_Workshop/Assets/Scripts/Object/Switch.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Switch : MonoBehaviour
 {
-    public string Door_Color; //문 특성
-    List <GameObject> Door = new List<GameObject>();
+    public string Switch_Color; //문 특성
+    bool IsUse = false;
     Animator anim;//애니메이션
     //Switch
 
@@ -18,12 +18,22 @@ public class Switch : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if(collision.gameObject.tag == "Bullet" || collision.gameObject.tag == "Player" || collision.gameObject.tag == "Player2")
         {
-            anim.Play("Switch");
+            if (IsUse)
+                return;
+            if(Switch_Color == "Red")
+                anim.Play("Red_Switch");
+            else if(Switch_Color == "Blue")
+                anim.Play("Blue_Switch");
             GameObject Door_p = GameObject.Find("Door");
             for (int i = 0; i < Door_p.transform.childCount; i++)
-                Door_p.transform.GetChild(i).GetComponentInChildren<Door>().FadeIn();
+            {
+                if(Door_p.transform.GetChild(i).GetComponentInChildren<Door>().Dore_Color == Switch_Color)
+                    Door_p.transform.GetChild(i).GetComponentInChildren<Door>().FadeIn();
+            }
+            IsUse = true;
         }
     }
 

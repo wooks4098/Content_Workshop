101f43b3439200b3a707d87cda745b3964d33082
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2020-11-25 15:54:24 +0900

    2챕터 배경
    구름 새

---
+++ Content_Workshop/Assets/Scripts/Back/Bird.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float speed;
+    public Transform pos;
+
+    private void Update()
+    {
+        gameObject.transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bird_Box")
+        {
+            gameObject.transform.position = pos.position + new Vector3(-39, 0, 0);
+        }
+    }
+}
---
+++ Content_Workshop/Assets/Scripts/Back/Cloud.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public float speed;
+    public Transform pos;
+
+    private void Update()
+    {
+        gameObject.transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Cloud_Box")
+        {
+            gameObject.transform.position = pos.position + new Vector3(39,0,0);
+        }
+    }
+
+}
